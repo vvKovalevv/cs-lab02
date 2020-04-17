1cb8d8ab5aef4b2c271ff2cfcf7c808f3f0e8e52
Alice (VVKovalevV) <ArTuR@mail.ru>
2020-04-17 23:15:17 +0300

    code:вывод максимума

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 using namespace std;
- int main ()
+int main ()
 {
     cout << "Enter A and B: " << '\n';
-int a, b;
-cin >> a >> b ;
-cout << "A + B = " << a + b << '\n';
-cout << "A - B = " << a - b << '\n';
-cout << "A * B = " << a * b << '\n';/*вывод произведения*/
-cout << "A / B = " << a / b << '\n';
+    int a, b, max;
+    cin >> a >> b ;
+    cout << "A + B = " << a + b << '\n';
+    cout << "A - B = " << a - b << '\n';
+    cout << "A * B = " << a * b << '\n';/*вывод произведения*/
+    cout << "A / B = " << a / b << '\n';
+    if (a>b)
+    {
+        cout<<"max="<<a<<'/n';
+    }
+    else
+    {
+        cout<< "max="<<b<<endl;
+    }
+
+    return 0;
 }
